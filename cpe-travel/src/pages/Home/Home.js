471dd1fe94d88692ac08176b1dbbae4fa8379078
Gabriel Mauad <gabrielbms711@gmail.com>
2020-10-19 22:54:20 -0300

    Correção no ultimo commit

--- cpe-travel/src/pages/Home/Home.js
+++ cpe-travel/src/pages/Home/Home.js
@@ -5,6 +5,7 @@ function Home() {
     return (
         <>
             <h1>Home</h1>
+            <h2 style={{ color: 'blue' }}> Grupo 4 cabuloso </h2>
             <Link style={{ marginRight: '10px' }} to="/Cadastro">Cadastro</Link>
             <Link to="/Login">Login</Link>
         </>
