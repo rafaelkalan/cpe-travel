d0a50b87442bdc6c418b7394e3715b6ae3fea13c
Fabrinni <fabrinnidias29@gmail.com>
2020-10-27 16:15:21 -0300

    Telas de Fundo
    Adicionada as telas de fundo do cadastro/Login

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import "./Register.css";
 
 function Register() {
   const history = useHistory();
 
   return (
-    <div>
+    <div className="containerRegister">
       <h1>Register</h1>
       <button type="button" onClick={() => history.goBack()}>Voltar</button>
     </div>
