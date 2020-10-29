0675a255c6798a0db6ad34da0ad8e14d780de73d
Jonas <jonasnetodias@gmail.com>
2020-10-29 19:07:21 -0300

    pagina footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Footer.css';
+//<footer className="footer"></footer>
+function Footer(){
+
+//<footer className="footer"></footer>
+
+const footerItens = [
+    {
+      path: '/',
+      text: 'Siga nos nas redes sociais',
+    },
+];
+return (
+    <div className="site-footer">  </div>
+
+
+)
+}
+export default Footer;
+
+
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 function Home() {
   return (
     <>
       <Header />
-      <h1>Home</h1>
+      
+      <h1>Home</h1> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+      <Footer />
     </>
   );
 }
