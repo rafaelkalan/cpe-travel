4a1f129d84e09e3a371997dfafe64ef4cf5dc1a9
Giovanni Martins <martinsgio95@gmail.com>
2020-11-05 21:54:38 -0300

    Alteracoes na box da home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Header from '../../components/Header';
 import Slider from '../../components/Carousel/Carousel';
+import './Home.css';
 
 function Home() {
   return (
     <>
       <Header />
       <Slider />
-      <h1>Home</h1>
+      <div className="boxSubtitle">
+        <h1>Guia de Destinos</h1>
+        <hr className="linha" />
+      </div>
+      <div className="boxMessage">
+        <p>Procure as melhores localidades e os preços mais baixos da viagem dos seus sonhos!</p>
+      </div>
     </>
   );
 }
