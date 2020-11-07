e68fed7ab9071f8f441b5eec4ad74e3b7f9047fe
Gabriel Mauad <gabrielbms711@gmail.com>
2020-11-07 16:59:39 -0300

    correçoes cards e carousel

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -29,12 +29,13 @@ function SliderCarousel() {
       prevIcon={getPrevArrowIcon()}
       onMouseOver={() => (setShow('visible'))}
       onMouseOut={() => (setShow('hidden'))}
+      className="boxCarousel"
     >
       {
         images.map((fotos) => (
           <Carousel.Item
             key={fotos.key}
-            interval={100000}
+            interval={3000}
           >
             <div className="imageCarousel" style={{ backgroundImage: `url(${fotos.carousel})` }} />
           </Carousel.Item>
