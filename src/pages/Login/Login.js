0c0d34c365455ee8ec07ce5e276935e14a077064
Jonas <jonasnetodias@gmail.com>
2020-11-13 15:20:37 -0300

    login e cadastro funcional

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import {useState} from "react";
 import { useHistory } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
 import './Login.css';
 
 function Login() {
   const history = useHistory();
-
-  const input = [
+  const [email, setEmail] = useState();
+  const [password, setPassord] = useState();
+  /*const input = [
     { controlId: 'email', type: 'email', placeholder: 'Digite o seu email' },
     { controlId: 'senha', type: 'password', placeholder: 'Digite a sua senha' },
-  ];
+  ];*/
 
+  function login(){
+    alert("Bem Vindo! \n" + email);
+    history.push("Home");
+    }
+
+    function regis(){
+
+      alert("Insira seus dados novamente para o email cadastrado:\n" + email);
+      history.push("cadastro");
+    }
   return (
     <div className="containerLogin">
       <div className="container">
@@ -20,23 +32,33 @@ function Login() {
           <p className="subtitle">
             Faça login para conferir ofertas exclusivas e os detalhes da sua viagem!
           </p>
-          <Form>
-            {input.map((item) => (
-              <Form.Group controlId={item.controlId}>
-                <Form.Control type={item.type} placeholder={item.placeholder} />
+          <Form>agora
+            
+              <Form.Group controlId="email">
+                <Form.Control type="email" 
+                placeholder="Email" 
+                onChange={(e)=>setEmail(e.target.value)}/>
               </Form.Group>
-            ))}
+            
+              
+                <Form.Group controlId="senha">
+                  <Form.Control type="password" 
+                  placeholder="Senha" 
+                  onChange={(e)=>setPassord(e.target.value)}/>
+                      </Form.Group>
+                    
+            
             <div className="fundocontainer1">
-              <Button variant="link">Esqueceu a senha?</Button>
+              <Button variant="link" onClick={regis}>Esqueceu a senha?</Button>
             </div>
-            <Button variant="primary" size="lg">Próximo</Button>
+            <Button variant="primary" size="lg" onClick={login}>Próximo</Button>  
             {' '}
             <div className="fundocontainer2">
               <p className="linkRegister">Não tem uma conta?</p>
-              <Button variant="link" onClick={() => history.push('/cadastro')}>Cadastre-se</Button>
+              <Button variant="history" onClick={() => history.push('/cadastro')}>Cadastre-se</Button>
             </div>
           </Form>
-                  </div>
+                </div>
       </div>
     </div>
   );
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import {useState} from "react";
 import { Form, Button } from 'react-bootstrap';
 import './Register.css';
 
 function Register() {
   const history = useHistory();
-
+  const [email, setEmail] = useState();
+  const [password, setPassord] = useState();
   const input = [
     { controlId: 'email', type: 'email', placeholder: 'Digite o seu email' },
     { controlId: 'email2', type: 'email', placeholder: 'Confirme o seu email' },
     { controlId: 'senha', type: 'password', placeholder: 'Digite a sua senha' },
     { controlId: 'senha2', type: 'password', placeholder: 'Confirme a sua senha' }];
 
+    function login(){
+      alert("Bem Vindo! \n" + email);
+      history.push("Home");
+    }
   return (
     <div className="containerRegister">
       <div className="container">
@@ -32,7 +38,7 @@ function Register() {
               Ao se cadastrar, você aceita os Termos de Serviço e a Política de
               Privacidade
             </p>
-            <Button variant="primary">Próximo</Button>
+            <Button variant="primary"  onClick={login}>Próximo</Button>
             {' '}
             <div className="fundocontainer">
               <p className="linkLogin">Já tem uma conta?</p>
@@ -46,5 +52,4 @@ function Register() {
     </div>
   );
 }
-
 export default Register;
