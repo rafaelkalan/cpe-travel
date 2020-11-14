8d6cf9a5830c9a54c6d4e727a0f822dafbe4c277
Giovanni Martins <martinsgio95@gmail.com>
2020-11-14 19:12:54 -0300

    Instalação da tela de cadastro de local, e uma pequena modificação nos Routes

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import RegisterLocal from './pages/RegisterLocal';
 import Local from './pages/Local';
 
 function Routes() {
@@ -11,7 +12,8 @@ function Routes() {
       <Switch>
         <Route exact path="/login" component={Login} />
         <Route exact path="/cadastro" component={Register} />
-        <Route exact path="/local" component={ Local} />
+        <Route exact path="/cadastro-local" component={RegisterLocal} />
+        <Route exact path="/local" component={Local} />
         <Route path="/" component={Home} />
       </Switch>
     </BrowserRouter>
